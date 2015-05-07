b5cc13eed0e3c82913ce14362f2b5eaf72861862
JimQiao <qiaoshiju@163.com>
2015-05-07 16:51:57 +0800

    增强图片读取方法

--- js/lib.js
+++ js/lib.js
@@ -50,3 +50,79 @@ function openImg(sourceId, targetId) {
     }
     reader.readAsDataURL(document.getElementById(sourceId).files[0]);
 }
+
+function isIE() {
+    var agent = window.navigator.userAgent;
+    var isIE7 = agent.indexOf('MSIE 7.0') != -1;
+    var isIE8 = agent.indexOf('MSIE 8.0') != -1;
+    if (isIE7) console.log("isIE7");
+    if (isIE8) console.log("isIE8");
+    return (isIE7 || isIE8);
+}
+function readFileInIE(filePath) {
+    try {
+        var fso = new ActiveXObject("Scripting.FileSystemObject");
+        var file = fso.GetFile(filePath);
+        var fileContent = fso.OpenTextFile(filePath, 1).ReadAll();
+        return {
+            name: file.Name,
+            type: file.Type,
+            size: file.Size,
+            content: fileContent
+        };
+    } catch (e) {
+        if (e.number == -2146827859) {
+            alert('Unable to access local files due to browser security settings. ' +
+            'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
+            'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+        }
+    }
+}
+
+function IEReader(sourceId, targetId, onloadend) {
+    var o = $(sourceId);
+    var img = $(targetId);
+    var bean = readFileInIE(o.value);
+    $(targetId).src = bean.content;//动态创建img
+    if (onloadend) {
+        onloadend({
+            target: img,
+            name: bean.name,
+            type: bean.type,
+            size: bean.size
+        });
+    }
+}
+function html5Reader(sourceId, targetId, onloadend) {
+    if (typeof FileReader === 'undefined') {
+        return IEReader(sourceId, targetId, onloadend);
+        //alert('Your browser does not support FileReader...');
+        //return;
+    }
+    var reader = new FileReader();
+    var file = document.getElementById(sourceId).files[0];
+
+    reader.onloadend = function () {
+        var img = document.getElementById(targetId);
+        img.src = this.result;
+        if (onloadend) {
+            onloadend({
+                target: img,
+                name: file.name,
+                type: file.type,
+                size: file.size
+            });
+        }
+    }
+    reader.readAsDataURL(file);
+}
+function bytesToSize(bytes) {
+    if (bytes === 0) return '0 B';
+    var k = 1024;
+    var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
+}
+function $(id) {
+    return document.getElementById(id);
+}
